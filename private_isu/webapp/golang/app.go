9c5ed7a8e4c3924036ec22dd504dc264f4fe07b1
izumin5210 <m@izum.in>
2023-11-18 10:44:15 +0900

    fix initialize

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -157,8 +157,11 @@ func dbInitialize() {
 		"DELETE FROM users WHERE id > 1000",
 		"DELETE FROM posts WHERE id > 10000",
 		"DELETE FROM comments WHERE id > 100000",
+		"DELETE FROM post_htmls WHERE post_id > 100000",
 		"UPDATE users SET del_flg = 0",
 		"UPDATE users SET del_flg = 1 WHERE id % 50 = 0",
+		"UPDATE post_htmls SET user_del_flg = 0",
+		"UPDATE post_htmls SET user_del_flg = 1 WHERE user_id % 50 = 0",
 	}
 
 	for _, sql := range sqls {
